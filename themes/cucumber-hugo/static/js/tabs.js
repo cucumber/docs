8c6973b872cad10bd0211bf139d80b7f45e1fc3c
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-05-17 15:43:38 +0100

    Tabs working (but no default)

--- themes/cucumber-hugo/static/js/tabs.js
+++ themes/cucumber-hugo/static/js/tabs.js
@@ -1,3 +1,5 @@
+// Misc utilities from http://youmightnotneedjquery.com/
+
 function ready(fn) {
   if (document.readyState != 'loading'){
     fn();
@@ -17,13 +19,37 @@ function element(html) {
   return tmp.body.firstChild;
 };
 
+function addClass(el, className) {
+  if (el.classList)
+    el.classList.add(className);
+  else
+    el.className += ' ' + className;
+}
+
+function removeClass(el, className) {
+  if (el.classList)
+    el.classList.remove(className);
+  else
+    el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+}
+
+// Logic to build tabs (for Bulma)
+
 ready(function() {
-  each(document, '.tabs', function(codeTabs) {
-    var ul = codeTabs.querySelector('ul');
-    each(codeTabs, '.tab-pane', function(tabPane) {
+  each(document, '.tabs-container', function(tabsContainer) {
+    var tabs = tabsContainer.querySelector('.tabs')
+    var ul = tabs.querySelector('ul');
+    each(tabs, '.tab-pane', function(tabPane) {
       var title = tabPane.getAttribute('title')
       var li = element('<li><a>' + title + '</a></li>');
       ul.appendChild(li)
+      li.addEventListener('click', function() {
+        each(ul, 'li', function(inactive) { removeClass(inactive, 'is-active') })
+        addClass(li, 'is-active')
+
+        each(tabs, '.tab-pane', function(hidden) { addClass(hidden, 'is-hidden') })
+        removeClass(tabPane, 'is-hidden')
+      })
     })
   })
 })
